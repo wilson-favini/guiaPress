c30e902f85ef09476625284c9bff190f0d0c9a92
wilson favini <wilsonfavini@gmail.com>
2025-09-09 23:15:39 -0400

    adicionado o cadastro de uma tarefa

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const router = express.Router();
-
-router.get("/categories", (req, res) => {
-    res.send("rota de categorias");
-});
+const Category = require("./Category");
+const slugify = require("slugify");
 
 router.get("/admin/categories/new", (req, res) => {
     res.render("admin/categories/new");
+});
+
+router.post("/categories/save", (req, res) => {
+    let title = req.body.title;
+    console.log(title);
+    if(title != undefined || title != "" || title != " " || title != null){
+        Category.create({
+            title: title,
+            slug: slugify(title)
+        }).then(() => {
+            res.redirect("/admin/categories/new");
+        });
+    } else{
+        res.redirect("/");
+    }
 });
 
 module.exports = router;
