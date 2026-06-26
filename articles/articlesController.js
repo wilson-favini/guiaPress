a94fac2d281ada761e593f41eb881e5466fdd620
wilson favini <wilsonfavini@gmail.com>
2026-06-25 21:32:22 -0400

    adicionado uma rota para gravar artigos, mas ainda é necessário revisão

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Category = require("../categories/Category");
 
-router.get("/articles", (req, res) => {
+router.get("/admin/articles", (req, res) => {
     res.send("articles");
 });
 
@@ -10,6 +10,21 @@ router.get("/admin/articles/new", (req, res) => {
     Category.findAll().then(categories => {
         res.render("admin/articles/new", {categories: categories});
     })
+});
+
+router.post("articles/save", (req, res) => {
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    }).then(() => {
+        res.redirect("/admin/articles");
+    });
 });
 
 module.exports = router;
--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require("sequelize");
 
 const connection = new Sequelize("guiapress", "root", "123456", {
     host:"localhost",
-    dialect: "mysql"
+    dialect: "mysql",
+    timezone: "-04:00"
 });
 
 module.exports = connection;
